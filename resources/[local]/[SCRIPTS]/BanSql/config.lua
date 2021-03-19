06fc52fa70cc3fc8df208cc054d9a4396efda551
Marcel <67207947+szymczakovv@users.noreply.github.com>
2021-03-19 12:01:43 +0100

    Add files via upload

---
+++ resources/[local]/[SCRIPTS]/BanSql/config.lua
@@ -0,0 +1,47 @@
+Config                   = {}
+
+Config.Lang              = 'en'    
+Config.permission        = "admin" 
+Config.ForceSteam        = true   
+Config.MultiServerSync   = false   
+
+Config.EnableDiscordLink = true --Turn this true if you want link the log to a discord (true-false)
+Config.webhookban        = "https://discord.com/api/webhooks/802180910137081866/C6Bva0yHc1_3actnud1jbMAbVXojvCGyeAe69-cFelAXWIGoNGiqEKcHntlRfCCQpfM3"
+Config.webhookunban      = "https://discord.com/api/webhooks/802180810269655082/J79v9CuBPgqPkqsE6ufFOD80lauHElS9xiI1yBNqoN1yQKaSxF8mfGMjtu2PzONnRhJ6"
+
+Config.TextEn = {
+    start         = "Lista banów oraz historia banów załadowała się poprawnie.",
+    starterror    = "Błąd: Lista banów oraz historia banów nie załadowała się, spróbuj ponownie.",
+    banlistloaded = "Lista banów załadowała się poprawnie.",
+    historyloaded = "Historia banów załadowała się poprawnie.",
+    loaderror     = "Błąd: Lista banów nie załadowała się.",
+    forcontinu    = " dni. Aby kontynuować wpisz /sqlreason [powód]",
+    noreason      = "Nie podano powodu.",
+    during        = " Długość: ",
+    noresult      = "Nie odnaleziono wyników.",
+    isban         = " został zbanowany",
+    isunban       = " został odbanowany",
+    invalidsteam  = "Steam jest wymagany aby dołączyć do serwera",
+    invalidid     = "Nie odnaleziono ID gracza.",
+    invalidname   = "Wpisana nazwa jest niepoprawna.",
+    invalidtime   = "Niepoprawna długość bana",
+    yourban       = "Zostałeś zbanowany za: ",
+    yourpermban   = "Zostałeś zbanowany permamentnie za: ",
+    youban        = "Zostałeś zbanowany za: ",
+    forr          = " dni. Za: ",
+    permban       = " permamentnie za: ",
+    timeleft      = ". Zostało: ",
+    toomanyresult = "Za dużo wyników, sprecyzuj kogo szukasz.",
+    day           = " dni ",
+    hour          = " godziny ",
+    minute        = " minuty ",
+    by            = "przez",
+    ban           = "Zbanuj gracza",
+    banoff        = "Zbanuj gracza (Offline)",
+    dayhelp       = "Długość (dni) bana",
+    reason        = "Powód bana",
+    history       = "Pokaż historię banów",
+    reload        = "Odśwież listę banów.",
+    unban         = "Odbanuj gracza.",
+    steamname     = "Nazwa steam"
+}
---
+++ resources/[local]/[SCRIPTS]/RealisticVehicleFailure/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version "bodacious"
+games {"gta5"}
+
+lua54 'yes'
+
+client_scripts {
+	"config.lua",
+	"client.lua"
+}
+
+server_scripts {
+	"config.lua",
+	"server.lua"
+}
+
+
+
+
