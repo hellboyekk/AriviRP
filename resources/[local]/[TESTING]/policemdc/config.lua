3f28cf3a575b33ae1e6ac92c810b4e00e6cef2d3
Marcel <67207947+szymczakovv@users.noreply.github.com>
2021-03-19 11:56:23 +0100

    Add files via upload

---
+++ resources/[local]/[TESTING]/policemdc/config.lua
@@ -0,0 +1,17 @@
+Config                            = {}
+
+Config.expirationCacheMillis = 30 * 60 * 1000 -- 30min
+Config.departments = {
+    police= {
+        logo= "lspdLogo.png",
+		displayName= "Los Santos Police Department"
+	},
+	statepolice= {
+		logo= "bcsoLogo.png",
+		displayName= "Blaine County Sheriff Office"
+	},
+	sheriff= {
+		logo= "bcsoLogo.png",
+		displayName= "Blaine County Sheriff Office"
+	}
+}
